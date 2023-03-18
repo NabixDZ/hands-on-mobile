21704b0e5c81d0a762ea5ccf92016695b7d75cb7
zakaria-blaili <zakaria07blaili@gmail.com>
2023-03-18 07:27:32 +0100

    !!

---
+++ lib/screens/feed.dart
@@ -0,0 +1,95 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/event.dart';
+import 'HomePage/widgets/postHome.dart';
+
+const uri = "http://192.168.43.43:5000";
+class Feed extends StatelessWidget {
+  const Feed({super.key});
+ Future<List<Event>> getEvent()async{
+       try {
+                final res = await http.get(Uri.parse("$uri/api/events/all") , 
+              
+                
+                );
+
+                final events = jsonDecode(res.body) as List ; 
+                print(events);
+                return events.map((e) => Event.fromMap(e)).toList();
+
+       } catch (e) {
+        print(e);
+        return [];
+         
+       }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+      child: ListView(
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: Image.asset("assets/images/profile.png",)),
+            SizedBox(height: 15,),
+            RichText(text: TextSpan(
+              
+              text: "Take a look at other people's",
+              style: TextStyle(
+                fontSize: 24,
+                        color: Colors.black,
+                        
+                      ),
+              children: [
+                TextSpan(text: " experience 🌟😁",style: TextStyle(
+                  fontSize: 24,
+                        color: Color.fromARGB(255, 255, 42, 0),
+                        
+                      ),)
+
+            ])),
+            SizedBox(height: 15,),
+            FutureBuilder(
+                future: getEvent(),
+                builder: (context, snapshot)  {
+                if(snapshot.hasData) {
+                  List<Event>? events = snapshot.data;
+                  if(events != null) {
+                    return GridView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        mainAxisExtent: 300,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 0,
+                        crossAxisCount: 2,
+                        childAspectRatio: 3/4
+                      ),
+                      itemCount: events.length,
+                      itemBuilder: ((context, index) => PostHome(userName: "userName", event: events[index])),
+                        
+                      );
+
+                  }
+                   return Text('Error');
+                  
+                }
+              
+                else{
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+
+                }
+              },),
+        ],
+      ),
+    );
+  }
+}
